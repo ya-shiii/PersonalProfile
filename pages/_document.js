b1f22571550c3327525b086f088f02a52eb14a80
Joshua Vicente <zho.xhie0630@gmail.com>
2024-03-25 10:18:41 +0800

    deploy

--- pages/_document.js
+++ pages/_document.js
@@ -9,11 +9,11 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href="/img/apple-icon.png" />
+          <link rel="shortcut icon" href="img/apple-icon.png" />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/img/apple-icon.png"
+            href="img/apple-icon.png"
           />
           {/* Fonts and icons */}
           <link
